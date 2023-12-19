b2c60f657b6bee26df9a68b6b30e86716ebf0ff6
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-12-19 21:52:15 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Page404 from './Pages/Page404'
+import MyNavbar from './components/MyNavbar'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Home from './Pages/Home'
+import "./App.css"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <>
+  <MyNavbar/>
+        <Routes>
+          <Route path='/' element={ <Home/>} />
+          <Route path='*' element={<Page404/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+
+  </>
 }
 
 export default App
